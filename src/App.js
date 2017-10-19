b9843e47dd5e6dedb8c76e2fe6ddfbcf094f1fcc
benhayehudi <bengreenberg@gmail.com>
2017-10-19 09:17:23 -0400

    working on custom navbar css stylesheet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import logo from './static_resources/images/pitgama_header.png';
 import './App.css';
+import './static_resources/stylesheets/Navbar.css'
 
 class App extends Component {
     render() {
         return (
           <div className="App">
-            <header className="App-header" >
+            <header className="App-header">
               <img src={logo} className="App-logo" alt="pitgama logo" />
+              <nav className="nav-container">
+                <ul>
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">About</a></li>
+                  <li><a href="#">Contact</a></li>
+                </ul>
+              </nav>
               <h1 className="App-title">Welcome to pitgama</h1>
             </header>
             <p className="App-intro">
