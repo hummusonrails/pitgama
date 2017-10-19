248c75701fdae4140b14bcc51245d9f142230abf
benhayehudi <bengreenberg@gmail.com>
2017-10-19 08:58:45 -0400

    beginning with file structure of app, added static resources including images and stylesheets

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './static_resources/images/pitgama_header.png';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+          <div className="App">
+            <header className="App-header" >
+              <img src={logo} className="App-logo" alt="pitgama logo" />
+              <h1 className="App-title">Welcome to pitgama</h1>
+            </header>
+            <p className="App-intro">
+              Some filler text.
+            </p>
+          </div>
+        );
+    }
 }
 
 export default App;
