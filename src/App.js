0a719aaab40b0389e677ee07d2d5aba5d9eea351
benhayehudi <bengreenberg@gmail.com>
2017-10-20 08:03:18 -0400

    created feature display area and footer styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './static_resources/images/pitgama_header.png';
 import './static_resources/stylesheets/Main.css'
 import './static_resources/stylesheets/Navbar.css'
+import './static_resources/stylesheets/Footer.css'
 
 class App extends Component {
     render() {
@@ -17,13 +18,29 @@ class App extends Component {
                 </ul>
               </nav>
             </header>
-            <div className="body-container">
+            <div className="splash-container">
               <div className="intro-box">
                 <h1 className="body-title">Welcome to piṯḡāmā</h1>
                 <p className="body-intro">
                   Data driven insights in Jewish text
                 </p>
                 <button className="banner-button"><a href="#">Get Started</a></button>
+              </div>
+            </div>
+            <div className="feature-display-container">
+              <div className="feature-1">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam velit metus, vulputate in consectetur et, porttitor ut nibh. Aliquam rhoncus enim tristique magna auctor hendrerit. Vestibulum cursus cursus mauris, ut faucibus ligula sollicitudin venenatis. Nulla facilisi. Sed commodo mauris sed mauris efficitur dictum. Nam suscipit risus eu arcu vulputate tempor. Maecenas varius, lorem at cursus consequat, augue tortor suscipit augue, id vulputate nibh mauris nec eros. Nulla facilisi. Morbi pulvinar varius velit, aliquet malesuada ex dignissim ut.
+              </div>
+              <div className="feature-2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam velit metus, vulputate in consectetur et, porttitor ut nibh. Aliquam rhoncus enim tristique magna auctor hendrerit. Vestibulum cursus cursus mauris, ut faucibus ligula sollicitudin venenatis. Nulla facilisi. Sed commodo mauris sed mauris efficitur dictum. Nam suscipit risus eu arcu vulputate tempor. Maecenas varius, lorem at cursus consequat, augue tortor suscipit augue, id vulputate nibh mauris nec eros. Nulla facilisi. Morbi pulvinar varius velit, aliquet malesuada ex dignissim ut.
+              </div>
+              <div className="feature-3">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam velit metus, vulputate in consectetur et, porttitor ut nibh. Aliquam rhoncus enim tristique magna auctor hendrerit. Vestibulum cursus cursus mauris, ut faucibus ligula sollicitudin venenatis. Nulla facilisi. Sed commodo mauris sed mauris efficitur dictum. Nam suscipit risus eu arcu vulputate tempor. Maecenas varius, lorem at cursus consequat, augue tortor suscipit augue, id vulputate nibh mauris nec eros. Nulla facilisi. Morbi pulvinar varius velit, aliquet malesuada ex dignissim ut.
+              </div>
+            </div>
+            <div className="footer-container">
+              <div className="footer-content">
+                FOOTER CONTENT HERE
               </div>
             </div>
           </div>
