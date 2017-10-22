d3504443cf0a600085ba85dbccdf716f806cb8ca
benhayehudi <bengreenberg@gmail.com>
2017-10-21 21:02:10 -0400

    cors issue

--- src/actions/api/ApiActions.js
+++ src/actions/api/ApiActions.js
@@ -1,6 +1,13 @@
+var myHeaders = new Headers({
+  'Content-Type': 'text/json',
+  'Origin': 'https://pitgama.herokuapp.com'
+});
+
 export function pitgamaSearch() {
     return (dispatch => {
-        fetch(`https://www.sefaria.org/api/texts/Sanhedrin?pad=0`)
+        fetch(`https://crossorigin.me/https://www.sefaria.org/api/texts/Sanhedrin?pad=0`, {
+          headers: myHeaders
+          })
           .then(data => data.json())
           .then(data => dispatch({ type: 'GET_RESULTS', data: data }))
     })
