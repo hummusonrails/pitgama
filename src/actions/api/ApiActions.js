25f73f564a80af7cf27d02890136c3791b10bc05
benhayehudi <bengreenberg@gmail.com>
2017-10-23 13:18:50 -0400

    updated contact contact & styling

--- src/actions/api/ApiActions.js
+++ src/actions/api/ApiActions.js
@@ -1,7 +1,7 @@
 
 export function pitgamaSearch() {
     return (dispatch => {
-        fetch(`https://jsonp.herokuapp.com?url=http://www.sefaria.org/api/texts/Sanhedrin?pad=0&commentary=0`)
+        fetch(`https://jsonp.herokuapp.com?url=http://www.sefaria.org/api/texts/Sanhedrin.38b?pad=0&commentary=0`)
           .then(data => data.json())
           .then(data => dispatch({ type: 'GET_RESULTS', data: data }))
     })
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,6 +9,7 @@ class Contact extends React.Component {
 
     return (
       <div>
+
         <div className="splash-container">
           <div className="intro-box">
             <h1 className="body-title">Contact</h1>
@@ -17,22 +18,56 @@ class Contact extends React.Component {
             </p>
           </div>
         </div>
+
         <div className="feature-display-container">
           <div className="feature-box">
-          <div className="bio_box">
-            <span id="bio_pic"><img src={ben_pic}/></span>
+
+          <div className="bio-container">
+            <div className="image-container">
+              <div className="image-content">
+                <img src={ben_pic}/>
+              </div>
+            </div>
+            <div className="right-container">
+              <div className="right-content">
+                <h4>Ben</h4>
+                <p>I am a full stack developer who is passionate about code and about achieving excellence in all aspects of the work. I bring nearly a decade of experience in organizational leadership with me to the work adding depth and perspective to any team. I work in backend development with Ruby on Rails, APIs, identity access management and SQL (PostgreSQL, MySQL, etc.) and I work in frontend development with Javascript, React and Redux, HTML and CSS. I learn new languages and frameworks quickly and I am committed to using the tech stack that is most ideal for the project.</p>
+              </div>
+            </div>
           </div>
-          <br />
-          <div className="bio_box">
-            <span id="bio_pic"><img src={malki_pic}/></span>
+
+          <div className="bio-container">
+            <div className="image-container">
+              <div className="image-content">
+                <img src={malki_pic}/>
+              </div>
+            </div>
+            <div className="right-container">
+              <div className="right-content">
+                <h4>Malki</h4>
+                <p>Background of IT, now venturing into the world of software engineering. Familiar with Ruby/Rails, Javascript, JQuery, React, Redux, Elixir, and many more on the list to learn!</p>
+              </div>
+            </div>
           </div>
-          <br />
-          <div className="bio_box">
-            <span id="bio_pic"><img src={yechiel_pic}/></span>
+
+          <div className="bio-container">
+            <div className="image-container">
+              <div className="image-content">
+                <img src={yechiel_pic}/>
+              </div>
+            </div>
+            <div className="right-container">
+              <div className="right-content">
+                <h4>Yechiel</h4>
+                <p>Brooklyn born and bred, I Got my rabbinical training in Israel. Went on to teach third grade in the Jewish Online School and adult education through Chabad. After a brief stint in tech support, I went on to study web development. I currently live in Brooklyn, NY with my family, supporting my passions for teaching, coding, and flying.</p>
+              </div>
+            </div>
           </div>
-          </div>
+
+
         </div>
       </div>
+    </div>
     );
   }
 }
