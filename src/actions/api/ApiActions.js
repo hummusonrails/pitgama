cfbd19ef47c33326db09a7a6d03c2f8cd8f4b630
benhayehudi <bengreenberg@gmail.com>
2017-10-25 18:18:34 -0400

    added fetch-jsonp module to access sefaria api without cors issue and without proxy

--- src/actions/api/ApiActions.js
+++ src/actions/api/ApiActions.js
@@ -2,7 +2,7 @@ import fetchJsonp from 'fetch-jsonp';
 
 export function pitgamaSearch() {
     return (dispatch => {
-        fetchJsonp(`http://www.sefaria.org/api/texts/Sanhedrin.38b?pad=0&commentary=0`)
+        fetchJsonp(`https://www.sefaria.org/api/texts/Sanhedrin.38b?pad=0&commentary=0`)
           .then(data => data.json())
           .then(data => dispatch({ type: 'GET_RESULTS', data: data }))
     })
