638684adf8fa8747f964c596aa15204cdf9d8b9d
benhayehudi <bengreenberg@gmail.com>
2017-10-29 21:14:13 -0400

    updated navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,11 +10,21 @@ class Header extends React.Component {
         <a href="/"><img src={logo} className="logo" alt="pitgama logo" /></a>
         <nav className="nav-container">
           <ul id="align-left">
-            <li id="align-left"><a href="/">Home</a></li>
-            <li id="align-left"><a href="#">Features</a></li>
-            <li id="align-left"><a href="#">Resources</a></li>
-            <li id="align-right"><a href="/about">About</a></li>
-            <li id="align-right"><a href="/contact">Contact</a></li>
+            <div>
+              <li id="align-left"><a href="/">Home</a></li>
+            </div>
+            <div className="dropdown">
+              <li id="align-left"><a href="#">Features</a></li>
+              <div className="dropdown-content">
+                <li id="align-left"><a href="/features/bruriah">Bruriah in the Talmud</a></li>
+              </div>
+            </div>
+            <div>
+              <li id="align-right"><a href="/about">About</a></li>
+            </div>
+            <div>
+              <li id="align-right"><a href="/contact">Contact</a></li>
+            </div>
           </ul>
         </nav>
       </header>
