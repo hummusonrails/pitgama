01e8c9c05513cc0ebf49cce9a5ee0d4ff6736df1
benhayehudi <bengreenberg@gmail.com>
2017-10-31 17:57:24 -0400

    added links back to sefaria website for resources

--- src/components/BruriahCard.js
+++ src/components/BruriahCard.js
@@ -32,14 +32,14 @@ class BruriahCard extends React.Component {
           <div className="texts-display-container">
 
             <div class="text-feature-card">
-              <div class="feature-card-header">{this.props.result.ref}</div>
+              <div class="feature-card-header"><a href={'https://www.sefaria.org/' + this.props.result.ref} target="_blank">{this.props.result.heSectionRef}</a></div>
               <div class="feature-card-main">
                 <div class="feature-main-description">{filteredHebrew}</div>
               </div>
             </div>
 
             <div class="text-feature-card">
-              <div class="feature-card-header">{this.props.result.heSectionRef}</div>
+              <div class="feature-card-header"><a href={'https://www.sefaria.org/' + this.props.result.heSectionRef} target="_blank">{this.props.result.ref}</a></div>
               <div class="feature-card-main">
                 <div class="feature-main-description">{strippedEnglish}</div>
               </div>
